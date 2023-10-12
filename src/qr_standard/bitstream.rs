08183202771a24493ecba031607876a9dcef3e1d
es <x@x.x>
2023-10-12 19:43:35 +0200

    more work on documenting modes and writing token-handling methods

--- src/qr_standard/bitstream.rs
+++ src/qr_standard/bitstream.rs
@@ -1,90 +1,76 @@
 // work in progress, suppressing warnings
 #![allow(unused_variables)]
 
+/*
+something that's really complicated is deciding what level of complexity/abstraction i want to tackle this problem at. there are really 4 different levels:
+1. raw input string
+2. raw substrings with mode indicators added
+3. (a vector of) individual tokens
+4. bits
+and i was stuck choosing between 2 and 3, where either option would make it really complicated to skip over the missing step. so i chose to do both
+*/
+
 #[derive(Clone)]
-enum Token {
+enum Mode {
     // not implementing ECI at this time
-
-    // mode indicator
-    // 4 bits
-    Mode(u8),
-
-    // character count
-    // 8 bits (i believe!!)
-    Count(u8),
 
     // ascii/shift-jis byte
     // 1 character / 8 bits
-    Byte(u8),
+    Byte,
 
     // base-10 numbers
-    // 3 numbers / 10 bits
-    // i'm not sure if the bit length is constant
-    Numeric(u16),
+    // 10, 7 or 4 bits
+    Numeric,
 
     // alphanumeric + 9 symbols (see notes)
     // 2 characters / 11 bits
-    AlphaNum(u16),
+    AlphaNum,
 
     // 1 character / 13 bits
-    Kanji(u16),
+    Kanji,
+}
+
+// level 2
+#[derive(Clone)]
+struct MarkedString {
+    mode: Mode,
+    string: String,
+}
+
+// level 3
+#[derive(Clone)]
+enum Token {
+    // mode and character count indicators,
+    // baked into one
+    ModeAndCount(Mode, u16),
+
+    // one character, which can vary in length
+    // between 4 and 13 bits
+    Character(Mode, u16),
 
     // the bit sequence 0000
     Terminator,
 }
 
-#[derive(Clone)]
-pub struct DataStream {
-    data: Vec<Token>,
-    // qr_version: u8,
-    // error_correction: u8,
-    // valid: Option<bool>,
-}
-
-#[derive(Clone)]
-pub struct BitData {
-    // undecided if i want format code too
-    format_code: u16,
-    codewords: Vec<u8>,
-}
-
-impl DataStream {
-    pub fn new() -> Self {
-        DataStream { data: Vec::new() }
-    }
-}
-
-impl BitData {
-    pub fn codewords(&self) -> &Vec<u8> {
-        &self.codewords
-    }
-}
-
-impl Token {
-    // number of bits taken up by a token
-    fn size(&self) -> usize {
+impl Mode {
+    // maximum number of bits taken up by a character
+    fn max_char_size(&self) -> usize {
         match self {
-            // not completely sure about this!
-            Self::Mode(_) => 4,
-            Self::Count(_) => 8,
-            Self::Byte(_) => 8,
-            Self::Numeric(_) => 10,
-            Self::AlphaNum(_) => 11,
-            Self::Kanji(_) => 13,
-            Self::Terminator => 4,
-            // _ => panic!(),
+            Self::Byte => 8,
+            Self::Numeric => 10,
+            Self::AlphaNum => 11,
+            Self::Kanji => 13,
         }
     }
 }
 
-// untested
-fn set_bit(input: &mut BitData, index: usize, state: bool) {
-    let (n, i) = (index / 8, index % 8);
-    if state {
-        // set a 1 (bitwise 'or' w/ 1)
-        input.codewords[n] |= 1 << i;
-    } else {
-        //set a 0 (bitwise 'and' w/ 0)
-        input.codewords[n] &= !(1 << i);
+impl Token {
+    // maximum number of bits taken up by a token
+    fn maxsize(&self) -> usize {
+        match self {
+            Self::ModeAndCount(..) => 20,
+            Self::Character(mode, _) => mode.max_char_size(),
+            Self::Terminator => 4,
+        }
     }
 }
