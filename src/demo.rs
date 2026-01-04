1610609048a497d6033b049890810d7fae396d9e
es <129210679+cool-es@users.noreply.github.com>
2026-01-04 02:00:37 +0100

    demo.rs: Simplify structure

--- src/demo.rs
+++ src/demo.rs
@@ -10,15 +10,16 @@ type NativeInt = i32;
 #[cfg(not(target_arch = "wasm32"))]
 type NativeInt = usize;
 
-#[cfg(target_arch = "wasm32")]
+type ByteVec = Vec<u8>;
+
 mod info {
-    use crate::demo::NativeInt;
+    use crate::demo::{ByteVec, NativeInt};
     use crate::qr_standard::bitstream::{Mode, Token};
 
     // the structure holding information about the qr code
     struct Info {
-        bitmap: Vec<u8>,
-        codewords: Vec<u8>,
+        bitmap: ByteVec,
+        codewords: ByteVec,
         mask: u8,
         version: u8,
     }
@@ -26,9 +27,9 @@ mod info {
     // the specific static variable storing the info at a specific place in memory
     static mut INFO_STATE: Info = Info {
         bitmap: Vec::new(),
-        codewords: todo!(),
+        codewords: Vec::new(),
         mask: u8::MAX,
-        version: todo!(),
+        version: u8::MAX,
     };
 
     // the unsafe "swiss army knife function"
@@ -41,88 +42,51 @@ mod info {
     }
 
     // returns pointer to and byte length of a vector
-    fn ptr_and_len<T>(ptr: bool, vec: &T) -> NativeInt
+    fn ptr_and_len<T>(vec: &'static T) -> (NativeInt, NativeInt)
     where
-        T: Into<Vec<u8>>,
+        ByteVec: From<&'static T>,
     {
-        let bytes: Vec<u8> = vec.into();
-        if ptr {
-            bytes.as_ptr() as NativeInt
-        } else {
-            bytes.len() as NativeInt
-        }
+        let bytes: ByteVec = vec.into();
+        (bytes.as_ptr() as NativeInt, bytes.len() as NativeInt)
     }
 
+    // operations to be called by the end user
     pub mod ops {
+        use super::*;
         use crate::demo::NativeInt;
 
         pub fn mask(mask: Option<u8>) -> u8 {
             if let Some(mask) = mask {
                 // set new value
                 process_info(|x| {
-                    let old = *x.mask;
+                    let old = x.mask;
                     x.mask = mask;
                     old
                 })
             } else {
                 // return existing value
-                process_info(|x| *x.mask)
+                process_info(|x| x.mask)
             }
+        }
+
+        pub fn bitmap(bitmap: ByteVec) {
+            // set new value
+            process_info(|x| {
+                x.bitmap = bitmap;
+            })
         }
 
         pub fn reset_all() {
             process_info(|x| {
                 *x = Info {
                     bitmap: Vec::new(),
-                    codewords: todo!(),
+                    codewords: Vec::new(),
                     mask: u8::MAX,
-                    version: todo!(),
+                    version: u8::MAX,
                 }
             })
         }
-
-        pub fn bitmap(k: bool) -> NativeInt {
-            process_info(|x| ptr_and_len(k, &x.bitmap))
-        }
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-mod info {
-    use crate::demo::NativeInt;
-    use std::cell::RefCell;
-
-    thread_local! {
-        // pub static CODEWORDS: RefCell<Vec<(String, u8)>> = RefCell::new(Vec::new());
-        // pub static VERSION_INFO: RefCell<Option<u32>> = RefCell::new(None);
-        // pub static VERSION: RefCell<Option<u8>> = RefCell::new(None);
-        pub static BITMAP: RefCell<Vec<u8>> = RefCell::new(Vec::new());
-        pub static MASK: RefCell<Option<u8>> = RefCell::new(None);
-    }
-
-    pub mod ops {
-        use super::*;
-        use crate::demo::NativeInt;
-
-        pub fn set_mask(mask: u8) {
-            MASK.with(|m| *m.borrow_mut() = Some(mask));
-        }
-    }
-
-    // pub fn set_version(version: u8) {
-    //     VERSION.with(|v| *v.borrow_mut() = Some(version));
-    // }
-
-    // pub fn set_codewords(codewords: Vec<(String, u8)>) {
-    //     CODEWORDS.with(|c| *c.borrow_mut() = codewords);
-    // }
-
-    // Getters
-    // pub fn get_mask() -> Option<u8> {
-    //     MASK.with(|m| *m.borrow())
-    // }
-
-    // etc...
-}
-
-use info::*;
+use info::ops::*;
