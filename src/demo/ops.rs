6875b87f8fb9b562f3915139ba9bac2b87b7ad5a
es <129210679+cool-es@users.noreply.github.com>
2026-01-04 10:29:36 +0100

    ops.rs: Improve set_bitmap()

--- src/demo/ops.rs
+++ src/demo/ops.rs
@@ -43,11 +43,8 @@ pub fn mask(mask: Option<NativeInt>) -> NativeInt {
 pub fn set_bitmap(bitmap: &Vec<Byte>) {
     // make 1 pixel per bit into 1 pixel per byte
     let bytes = bitmap.iter().flat_map(|x| {
-        let mut ax = [0; 8];
-        for (i, v) in ax.iter_mut().enumerate() {
-            *v = x.wrapping_shr(7 - i as u32) & 1;
-        }
-        ax.into_iter()
+        let f = |z: u8| u8::from(x & (1 << (7 - z)) == 0);
+        [f(0), f(1), f(2), f(3), f(4), f(5), f(6), f(7)].into_iter()
     });
 
     process_info(|x| {
