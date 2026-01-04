4a304e9ff16035d76f5969b58918d651c76df237
es <129210679+cool-es@users.noreply.github.com>
2026-01-04 18:31:32 +0100

    ops.rs: Add alternate bitmap function (WIP)

--- src/demo/ops.rs
+++ src/demo/ops.rs
@@ -51,6 +51,23 @@ pub fn set_bitmap(bitmap: &Vec<Byte>) {
     })
 }
 
+pub fn set_bitmap_2<F>(bitmap: &Vec<Byte>, ff: F)
+where
+    F: FnOnce(&mut Info) -> &mut Bitmap,
+{
+    // make 1 pixel per bit into 1 pixel per byte
+    let bytes = bitmap.iter().flat_map(|x| {
+        let f = |z: Byte| Byte::from(x & (1 << (7 - z)) == 0);
+
+        [f(0), f(1), f(2), f(3), f(4), f(5), f(6), f(7)].into_iter()
+    });
+
+    process_info(|x| {
+        ff(x).as_bmp();
+        x.bitmap.extend(bytes);
+    })
+}
+
 pub fn set_modes(modes: &Vec<(Mode, String)>) {
     // reduce modes vector into pairs of mode and character byte
     let modes = modes.iter().flat_map(|(m, s)| {
