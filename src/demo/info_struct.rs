85cbfeb70832679c9400d20db0000f940211ced1
es <129210679+cool-es@users.noreply.github.com>
2026-01-04 19:44:18 +0100

    info_structure.rs: Change visibility

--- src/demo/info_struct.rs
+++ src/demo/info_struct.rs
@@ -4,7 +4,7 @@ use super::{Byte, NativeInt};
 
 // the structure holding information about the qr code
 #[derive(Debug, Clone)]
-pub(crate) struct Info {
+pub struct Info {
     // bitmap without a mask
     pub bitmap_fcode: Vec<Byte>,
 
@@ -12,30 +12,30 @@ pub(crate) struct Info {
     pub bitmap_nomask: Vec<Byte>,
 
     // bitmap with a mask (readable qr code)
-    pub(crate) bitmap: Vec<Byte>,
+    pub bitmap: Vec<Byte>,
 
     // TBD
-    pub(crate) codewords: Vec<Byte>,
+    pub codewords: Vec<Byte>,
 
     // the small rectangle in larger qr codes
-    pub(crate) ecblock_data: Vec<Byte>,
+    pub ecblock_data: Vec<Byte>,
 
     // the mask data positioned around the alignment patterns
     pub format_info: [Byte; 2],
 
     // which mask was chosen
-    pub(crate) mask: NativeInt,
+    pub mask: NativeInt,
 
     // mode/byte data, as chosen by find_best_mode_optimization()
     // reprocessed into a format more readable by javascript
-    pub(crate) modes: Vec<Byte>,
+    pub modes: Vec<Byte>,
 
     // which version was chosen
-    pub(crate) version: NativeInt,
+    pub version: NativeInt,
 }
 
 impl Info {
-    pub(crate) const fn new() -> Info {
+    pub const fn new() -> Info {
         Info {
             bitmap_fcode: Vec::new(),
             bitmap_nomask: Vec::new(),
@@ -49,7 +49,7 @@ impl Info {
         }
     }
 
-    pub(crate) fn clear(&mut self) {
+    pub fn clear(&mut self) {
         self.bitmap_fcode.clear();
         self.bitmap_nomask.clear();
         self.bitmap.clear();
